97d64d765641063bdefdb08dc005a13d6577e648
Uliaaan <markovopg@gmail.com>
2016-10-24 22:33:55 +1000

    v0.02
    CSS форма входа

--- footer.php
+++ footer.php
@@ -1,5 +1,63 @@
 </div> <!-- end wrapper -->
 
+    <!-- </div> -->
+    <div class="main">
+        <div class="section section-white section-with-space">
+            <div class="container tim-container text-center">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 text-center">
+                        <h2>Completed with examples</h2>
+                        <p>The kit comes with three pre-built pages to help you get started faster. You can change the text and images and you're good to go. More importantly, looking at them will give you a picture of what you can built with this powerful kit.</p>
+                    </div>
+                </div>
+                <div class="row example-pages">
+                    <div class="col-md-4">
+                        <a href="examples/landing.html" target="_blank">
+                            <img src="assets/paper_img/examples/landing.jpg" alt="Rounded Image" class="img-rounded img-responsive">
+                            <h5>Landing Page</h5>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="examples/register.html" target="_blank">
+                            <img src="assets/paper_img/examples/register.jpg" alt="Rounded Image" class="img-rounded img-responsive">
+                            <h5>Register</h5>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="examples/profile.html" target="_blank">
+                            <img src="assets/paper_img/examples/profile.jpg" alt="Rounded Image" class="img-rounded img-responsive">
+                            <h5>Profile</h5>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section section-nude section-with-space">
+            <div class="container tim-container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 text-center">
+                        <h2>Do you like what you see?</h2>
+                        <p>Cause if you do, it can be yours for free. Hit the button below to navigate to Creative Tim where you can find the kit. Start a new project or give an old Bootstrap project a new look, we've got you covered. </p>
+                    </div>
+                    <div class="col-xs-8 col-xs-offset-2 col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 download-area">
+                        <a href="http://www.creative-tim.com/product/paper-kit" class="btn btn-danger btn-fill btn-block btn-lg">Download</a>
+                    </div>
+                </div>
+                <div class="row sharing-area text-center">
+                        <h3>Sharing is caring!</h3>
+                        <a href="#" class="btn">
+                            <i class="fa fa-twitter"></i>
+                            Twitter
+                        </a>
+                        <a href="#" class="btn">
+                            <i class="fa fa-facebook-square"></i>
+                            Facebook
+                        </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <footer class="footer-demo section-dark">
     <div class="container">
         <nav class="pull-left">
@@ -47,6 +105,57 @@
         <div class="right-side">
             <button type="button" class="btn btn-danger btn-simple">Delete</button>
         </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- MODAL ENTER -->
+
+<div class="modal fade" id="enter" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Форма входа</h4>
+      </div>
+      <div class="modal-body">
+            <input type="text" class="form-control" placeholder="Логин"><br> 
+            <input type="password" class="form-control" placeholder="Пароль"><br>
+        <div class="forgot text-center">
+            <a href="#" class="btn btn-simple btn-danger">Забыли пароль?</a>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <div class="left-side">
+            <a href="#" target="_blank" type="button" class="btn btn-danger btn-simple" data-dismiss="modal" data-toggle="modal" data-target="#register">Регистрация</a>
+        </div>
+        <div class="divider"></div>
+        <div class="right-side">
+            <button type="button" class="btn btn-default btn-simple" >Войти</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- MODAL REGISTER -->
+
+<div class="modal fade" id="register" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Регистрация</h4>
+      </div>
+      <div class="modal-body">
+            <input type="text" class="form-control" placeholder="Логин"><br>
+            <input type="email" class="form-control" placeholder="E-mail"><br>
+            <input type="password" class="form-control" placeholder="Пароль"><br>
+            <input type="password" class="form-control" placeholder="Повторите пароль">
+      </div>
+      <div class="modal-footer">
+            <button type="button" class="btn btn-default  btn-simple">Зарегистрироваться</button>
       </div>
     </div>
   </div>
